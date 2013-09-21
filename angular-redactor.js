4b7170134c2cefdbaab942a379ee2f721b7daf48
Christopher Hiller <chiller@badwing.com>
2013-09-21 12:34:21 -0700

    avoiding $$phase hack mostly, getting demo working
    - updating `bower.json` to include angular-routes
    - added `.bowerrc` for appropriate directory
    - modified script to not use the `$$phase` hack
    - removed `options` attribute in lieu of simply attaching options to
    the `redactor` attribute
    - put module in self-executing anonymous function
    - other slight tweaks

--- angular-redactor.js
+++ angular-redactor.js
@@ -1,47 +1,41 @@
-'use strict';
+(function () {
+  'use strict';
 
-/**
- * usage: <textarea ng-model="content" redactor></textarea>
- *
- *    additional options:
- *      options: hash (pass in a redactor options hash)
- *
- */
-angular.module('angular-redactor', [])
-  .directive("redactor", [function () {
-    return {
-      restrict: 'A',
-      require: "ngModel",
-      template: '<textarea cols="30" rows="10"></textarea>',
-      link: function (scope, elm, attrs, ngModel) {
-        scope.safeApply = function (fn) {
-          var phase = this.$root.$$phase;
-          if (phase == '$apply' || phase == '$digest') {
-            if (fn && (typeof(fn) === 'function')) {
-              fn();
-            }
-          } else {
-            this.$apply(fn);
-          }
-        }
+  /**
+   * usage: <textarea ng-model="content" redactor></textarea>
+   *
+   *    additional options:
+   *      options: hash (pass in a redactor options hash)
+   *
+   */
+  angular.module('angular-redactor', [])
+    .directive("redactor", ['$timeout', function ($timeout) {
+      return {
+        restrict: 'A',
+        require: "ngModel",
+        link: function (scope, elm, attrs, ngModel) {
+          var updateModel = function updateModel(value) {
+              scope.$apply(function () {
+                ngModel.$setViewValue(value);
+              });
+            }, options = {
+              changeCallback: updateModel
+            }, additionalOptions = angular.isDefined(attrs.options) ?
+                                   scope.$eval(attrs.options) : {},
+            editor;
+          angular.extend(options, additionalOptions);
 
-        var updateModel = function () {
-          return scope.safeApply(function () {
-            ngModel.$setViewValue(elm.redactor('get'));
+          $timeout(function () {
+            editor = elm.redactor(options);
           });
-        };
 
-        var options = {
-          changeCallback: updateModel
-        };
+          ngModel.$render = function () {
+            if (angular.isDefined(editor)) {
+              elm.redactor('set', ngModel.$viewValue || '')
+            }
+          };
+        }
+      };
+    }]);
+})();
 
-        var additionalOptions = attrs.options ? scope.$eval(attrs.options) : {};
-        angular.extend(options, additionalOptions);
-
-        var redactor = elm.redactor(options);
-        ngModel.$render = function () {
-          return redactor != null ? elm.redactor('set', ngModel.$viewValue || '') : void 0;
-        };
-      }
-    };
-  }]);
