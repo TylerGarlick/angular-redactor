0baa5bcd3418adf538d57043e621cd8f90e5c37f
Jake Zerrer <him@jakezerrer.com>
2015-04-20 13:27:36 -0400

    redactor directive takes new 'update-on' attribute

--- angular-redactor.js
+++ angular-redactor.js
@@ -17,35 +17,46 @@
             return {
                 restrict: 'A',
                 require: 'ngModel',
+                scope: {
+                    redactor: "@",
+                    updateOn: "@"
+                },
                 link: function(scope, element, attrs, ngModel) {
+
+                    // by default, assume we want to update the model based on changeCallback
+                    scope.updateOn = typeof scope.updateOn !== 'undefined' ? scope.updateOn : "changeCallback";
 
                     // Expose scope var with loaded state of Redactor
                     scope.redactorLoaded = false;
 
                     var updateModel = function updateModel(value) {
                             // $timeout to avoid $digest collision
+
+                            var code = this.code.get();
+
                             $timeout(function() {
                                 scope.$apply(function() {
-                                    ngModel.$setViewValue(value);
+                                    ngModel.$setViewValue(code);
                                 });
                             });
                         },
-                        options = {
-                            changeCallback: updateModel
-                        },
+                        options = {},
                         additionalOptions = attrs.redactor ?
                             scope.$eval(attrs.redactor) : {},
                         editor,
                         $_element = angular.element(element);
 
+                    options[scope.updateOn] = updateModel;
+
                     angular.extend(options, redactorOptions, additionalOptions);
 
                     // prevent collision with the constant values on ChangeCallback
-                    var changeCallback = additionalOptions.changeCallback || redactorOptions.changeCallback;
-                    if (changeCallback) {
-                        options.changeCallback = function(value) {
+                    var updateCallback = additionalOptions[scope.updateOn] || redactorOptions[scope.updateOn];
+
+                    if (updateCallback) {
+                        options[updateCallback] = function(value) {
                             updateModel.call(this, value);
-                            changeCallback.call(this, value);
+                            updateCallback.call(this, value);
                         }
                     }
 
